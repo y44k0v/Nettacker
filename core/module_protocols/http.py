2ae5e8b7c4c63555f1aa517f0cc5d5f1c870c18a
Ali Razmjoo <ali.razmjoo@owasp.org>
2021-12-19 23:12:59 +0100

    Revert "replace requests with urllib3"
    This reverts commit 8030c0d9be7bd9632f600b4628144022a67d18dc.

--- core/module_protocols/http.py
+++ core/module_protocols/http.py
@@ -98,14 +98,15 @@ class Engine:
             request_number_counter,
             total_number_of_requests
     ):
-        # backup_method = copy.deepcopy(sub_step['method'])
+        backup_method = copy.deepcopy(sub_step['method'])
         backup_response = copy.deepcopy(sub_step['response'])
-        import urllib3
+        import requests
         from requests.packages.urllib3.exceptions import InsecureRequestWarning
-        urllib3.disable_warnings(InsecureRequestWarning)
-        action = getattr(urllib3.PoolManager(), 'request', None)
+        requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+        action = getattr(requests, backup_method, None)
         if options['user_agent'] == 'random_user_agent':
             sub_step['headers']['User-Agent'] = random.choice(options['user_agents'])
+        del sub_step['method']
         del sub_step['response']
         if 'dependent_on_temp_event' in backup_response:
             temp_event = get_dependent_results_from_database(
@@ -131,9 +132,10 @@ class Engine:
                 break
             except Exception:
                 response = []
+        sub_step['method'] = backup_method
         sub_step['response'] = backup_response
         sub_step['response']['conditions_results'] = response_conditions_matched(sub_step, response)
-        del urllib3
+        del requests
         del action
         return process_conditions(
             sub_step,
